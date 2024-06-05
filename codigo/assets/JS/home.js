7b8b8ad1db49ef232788742d4458cdfed07a14ca
gmaroun1 <163938333+gmaroun1@users.noreply.github.com>
2024-06-04 23:03:25 -0300

    Update home.js

--- codigo/assets/JS/home.js
+++ codigo/assets/JS/home.js
@@ -23,8 +23,8 @@ function leDados () {
 function usuario() {
     let screen = document.getElementById('screen');
     let strHtmlHome = '';
-    strHtmlHome+= `<h1>Logado como ${usuarioLogado.email}</h1><br>`
-    strHtmlHome+= `<h3>Olá ${usuarioLogado.nome}</h3>`
+    strHtmlHome+= `<h1>Logado como ${usuarioLogado.email}</h1> <h3>Olá ${usuarioLogado.nome}</h3>`
+    strHtmlHome+= 
     screen.innerHTML = strHtmlHome;
 
 
@@ -32,6 +32,18 @@ function usuario() {
 
 usuario();
 
-    
-    
-    
+// Config dos botões
+
+document.getElementById('toggleSidebar').addEventListener('click', function() {
+    document.body.classList.toggle('sidebar-open');
+  });
+  
+document.getElementById('criar').addEventListener('click', function () {
+    this.style.display = 'none';
+
+    document.getElementById('mesa').style.display = 'block';
+});
+
+document.getElementById('mostraConta').addEventListener('click', function () {
+    this.style.display = 'block';
+});
