336a4b51a382455fd733810a2455da575288f80a
Gabriel Maroun <163938333+gmaroun1@users.noreply.github.com>
2024-07-01 23:13:22 -0300

    Add files via upload

---
+++ codigo/JS/carteira.js
@@ -0,0 +1,52 @@
+let saldo = 0;
+
+function showCarteira() {
+  if (document.getElementById('carteira').style.display == 'block') {
+    document.getElementById('carteira').style.display = 'none';
+    document.getElementById('optionsContainer').style.display = 'none';
+  }
+  else document.getElementById('carteira').style.display = 'block';
+}
+
+function showDeposit() {
+  document.getElementById('optionsContainer').style.display = 'block';
+}    
+
+function showInput(opcao) {
+  if (opcao === 'debito' || opcao === 'credito' || opcao === 'pix') {
+    document.getElementById('inputContainer').style.display = 'block';
+    document.getElementById('optionsContainer').style.display = 'none'; // Esconder as opções de pagamento ao mostrar o input
+}
+}
+
+
+function deposit() {
+  const valor = parseFloat(document.getElementById('valor').value);
+  if (isNaN(valor) || valor <= 0) {
+    alert('Por favor, insira um valor válido.');
+    return;
+  }
+  saldo += valor;
+  document.getElementById('saldo').innerText = `R$ ${saldo.toFixed(2)}`;
+  document.getElementById('valor').value = '';
+  document.getElementById('inputContainer').style.display = 'none';
+  document.getElementById('optionsContainer').style.display = 'none';
+  document.getElementById('carteira').style.display = 'none';
+  alert('Depósito realizado');
+}
+
+function confirm() {
+  if (saldo < valorCompartilhado) {
+    alert('Saldo insuficiente!');
+  }
+  else {
+    saldo = saldo - valorCompartilhado;
+    alert('Pagamento efetuado!');
+    console.log(saldo);
+    document.getElementById('saldo').innerText = `R$ ${saldo.toFixed(2)}`;
+    
+    if (document.getElementById('confirm').style.display == 'block') document.getElementById('confirm').style.display = 'none';
+    else document.getElementById('confirm').style.display = 'block';
+  }
+
+}
